1611939f8070df2da349b9fed8fb3ae6a4062423
201198al <201198al@gmail.com>
2026-02-17 21:46:12 +1000

    Исправлены баги

--- index.php
+++ index.php
@@ -46,6 +46,7 @@ $example_persons_array = [
         'job' => 'babysitter',
     ],
 ];
+
 function getPartsFromFullname($fullname) {
     $parts = explode(' ', $fullname);
     return [
@@ -58,10 +59,12 @@ function getPartsFromFullname($fullname) {
 function getFullnameFromParts($surname, $name, $patronomyc) {
     return $surname . ' ' . $name . ' ' . $patronomyc;
 }
+
 function getShortName($fullname) {
     $parts = getPartsFromFullname($fullname);
     return $parts['name'] . ' ' . mb_substr($parts['surname'], 0, 1) . '.';
 }
+
 function getGenderFromName($fullname) {
     $parts = getPartsFromFullname($fullname);
     $score = 0;
@@ -78,22 +81,7 @@ function getGenderFromName($fullname) {
     if ($score < 0) return -1;
     return 0;
 }
-function getGenderFromName($fullname) {
-    $parts = getPartsFromFullname($fullname);
-    $score = 0;
 
-    if (mb_substr($parts['patronomyc'], -3) === 'вна') $score--;
-    if (mb_substr($parts['name'], -1) === 'а') $score--;
-    if (mb_substr($parts['surname'], -2) === 'ва') $score--;
-
-    if (mb_substr($parts['patronomyc'], -2) === 'ич') $score++;
-    if (in_array(mb_substr($parts['name'], -1), ['й', 'н'])) $score++;
-    if (mb_substr($parts['surname'], -1) === 'в') $score++;
-
-    if ($score > 0) return 1;
-    if ($score < 0) return -1;
-    return 0;
-}
 function getGenderDescription($array) {
     $total = count($array);
     $male = $female = $undefined = 0;
@@ -114,6 +102,7 @@ function getGenderDescription($array) {
         . "Женщины - $femalePercent%\n"
         . "Не удалось определить - $undefinedPercent%";
 }
+
 function getPerfectPartner($surname, $name, $patronomyc, $array) {
     $surname = mb_convert_case($surname, MB_CASE_TITLE);
     $name = mb_convert_case($name, MB_CASE_TITLE);
@@ -133,22 +122,4 @@ function getPerfectPartner($surname, $name, $patronomyc, $array) {
 
     return "$shortName1 + $shortName2 = \n♡ Идеально на $percent% ♡";
 }
-function getPerfectPartner($surname, $name, $patronomyc, $array) {
-    $surname = mb_convert_case($surname, MB_CASE_TITLE);
-    $name = mb_convert_case($name, MB_CASE_TITLE);
-    $patronomyc = mb_convert_case($patronomyc, MB_CASE_TITLE);
 
-    $fullname = getFullnameFromParts($surname, $name, $patronomyc);
-    $gender = getGenderFromName($fullname);
-
-    do {
-        $partner = $array[array_rand($array)];
-        $partnerGender = getGenderFromName($partner['fullname']);
-    } while ($partnerGender === $gender || $partnerGender === 0);
-
-    $shortName1 = getShortName($fullname);
-    $shortName2 = getShortName($partner['fullname']);
-    $percent = number_format(mt_rand(5000, 10000) / 100, 2, '.', '');
-
-    return "$shortName1 + $shortName2 = \n♡ Идеально на $percent% ♡";
-}
